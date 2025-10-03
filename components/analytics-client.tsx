d41393c96a581120b5852caf2dc9928cfb0f9704
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-10-04 01:13:07 +0200

    usd to kwd

--- components/analytics-client.tsx
+++ components/analytics-client.tsx
@@ -157,7 +157,7 @@ export function AnalyticsClient({ initialData }: AnalyticsClientProps) {
   const formatCurrency = (value: number) => {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
-      currency: 'USD',
+      currency: 'KWD',
       minimumFractionDigits: 0,
       maximumFractionDigits: 0
     }).format(value)
